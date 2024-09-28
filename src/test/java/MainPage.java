149701306e260bccf5a404c7a49d31f722e68df8
Александр <lazzero@MacBook-Air.local>
2024-09-28 21:53:35 +0300

    Created a constructor for opening url and created search method for to write a query and press the Enter key

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -4,17 +4,23 @@
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
 import static com.codeborne.selenide.Selenide.$x;
 
 public class MainPage {
-    private final SelenideElement searchButton = $x("//form");
+    private final SelenideElement textBoxInput = $x("//input[@type='text']");
 
-    public void clickOnSearch() {
-        searchButton.click();
+    public MainPage(String url) {
+        Selenide.open(url);
     }
 
-    public void openWebSite(String url) {
-        Selenide.open(url);
+    /*
+    Automatically searches the input field and presses the Enter key
+     */
+    public void search(String searchString) {
+        textBoxInput.setValue(searchString);
+        textBoxInput.sendKeys(Keys.ENTER);
     }
 
 }
